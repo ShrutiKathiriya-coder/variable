858c7d3b2ee14d2df1071dc40e5695fae7a35c5e
ShrutiKathiriya-coder <kathiriyashruti3@gmail.com>
2024-11-20 18:27:42 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,46 @@
+//variable
+
+// defination : like this container store to information or data 
+
+//initalization
+
+var a;
+ let x;
+ const pi=3.14    //asign
+
+ // datatypes :
+
+ //primitive datatypes:
+  -Number
+  -String
+  -Boolean
+  -null
+  -undefined
+  -Symbol
+  -BigInt
+  
+
+  let num=10;
+  console.log(num);
+  console.log(typeof num);
+
+  var str="shruti"
+  console.log(str);
+  console.log(typeof str);
+
+  var bool=true
+  console.log(bool)
+  console.log(typeof bool)
+
+  var nullvar=undefined
+  console.log(nullvar)
+  console.log(typeof nullvar)
+
+
+  //non-primitive datatypes:
+
+  -Object
+
+  var obj ={};
+  console.log(obj);
+  
